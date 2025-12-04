091a6298178fc35c74fd05ce1149be0bf567d1bc
desarrollovolcan <eisla@fvolcan.cl>
2025-12-04 18:18:19 -0300

    Actualizacion informes

--- assest/documento/informeIcargaInvoicev2.php
+++ assest/documento/informeIcargaInvoicev2.php
@@ -43,6 +43,7 @@ include_once '../../assest/controlador/TFLETE_ADO.php';
 
 include_once '../../assest/controlador/TCONTENEDOR_ADO.php';
 include_once '../../assest/controlador/ATMOSFERA_ADO.php';
+include_once '../../assest/controlador/EMISIONBL_ADO.php';
 include_once '../../assest/controlador/PAIS_ADO.php';
 include_once '../../assest/controlador/SEGURO_ADO.php';
 
@@ -104,6 +105,7 @@ $CVENTA_ADO =  new CVENTA_ADO();
 $TFLETE_ADO =  new TFLETE_ADO();
 $TCONTENEDOR_ADO =  new TCONTENEDOR_ADO();
 $ATMOSFERA_ADO =  new ATMOSFERA_ADO();
+$EMISIONBL_ADO =  new EMISIONBL_ADO();
 $SEGURO_ADO =  new SEGURO_ADO();
 
 $EEXPORTACION_ADO = new EEXPORTACION_ADO();
@@ -341,11 +343,11 @@ if($ARRAYICARGA){
       $BOOKINGINSTRUCTIVO = $ARRAYICARGA[0]['BOOKING_ICARGA'];
       $TEMBARQUE = $ARRAYICARGA[0]['TEMBARQUE_ICARGA'];
       $FECHAETD = $ARRAYICARGA[0]['FECHAETD'];
-      $FECHAETA = $ARRAYICARGA[0]['FECHAETA'];    
-      $BOLAWBCRTINSTRUCTIVO = $ARRAYICARGA[0]['ID_EMISIONBL'];
-      if(!$BOLAWBCRTINSTRUCTIVO){
-        $BOLAWBCRTINSTRUCTIVO = $ARRAYICARGA[0]['BOLAWBCRT_ICARGA'];
-      }
+      $FECHAETA = $ARRAYICARGA[0]['FECHAETA'];
+        $BOLAWBCRTINSTRUCTIVO = $ARRAYICARGA[0]['CRT_ICARGA'];
+        if(!$BOLAWBCRTINSTRUCTIVO){
+          $BOLAWBCRTINSTRUCTIVO = "Sin Datos";
+        }
 
 
       $TINSTRUCTIVO = $ARRAYICARGA[0]['T_ICARGA'];
