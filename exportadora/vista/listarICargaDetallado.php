dd9c4e02206e9553ec497fcf15cc1223da9996b1
desarrollovolcan <eisla@fvolcan.cl>
2025-12-11 00:17:04 -0300

    Add BL number column to ICarga detailed views

--- exportadora/vista/listarICargaDetallado.php
+++ exportadora/vista/listarICargaDetallado.php
@@ -233,6 +233,7 @@ include_once "../../assest/config/datosUrLP.php";
                                                     <tr class="text-center">
                                                         <th>Número </th>
                                                         <th>Número Referencia</th>
+                                                        <th>N° de BL</th>
                                                         <th>Codigo Estandar </th>
                                                         <th>Envase/Estandar </th>
                                                         <th>Peso Neto </th>
@@ -545,6 +546,7 @@ include_once "../../assest/config/datosUrLP.php";
                                                             <tr class="text-center">
                                                                 <td> <?php echo $r['NUMERO_ICARGA']; ?>  </td>
                                                                 <td> <?php echo $r['NREFERENCIA_ICARGA']; ?>  </td>
+                                                                <td> <?php echo $r['BLAWB']; ?> </td>
                                                                 <td> <?php echo $CODIGOESTANDAR; ?></td>
                                                                 <td> <?php echo $NOMBREESTANTAR ?></td>
                                                                 <td> <?php echo number_format($NETOESTANTAR, 2, ".", "") ?></td>
