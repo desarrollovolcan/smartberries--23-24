f96391e5e92a8cfd665e65dca32b1502b0105782
desarrollovolcan <eisla@fvolcan.cl>
2025-12-11 00:31:22 -0300

    Remove BL/AWB column from ICarga detallado

--- exportadora/vista/listarICargaDetallado.php
+++ exportadora/vista/listarICargaDetallado.php
@@ -233,6 +233,7 @@ include_once "../../assest/config/datosUrLP.php";
                                                     <tr class="text-center">
                                                         <th>Número </th>
                                                         <th>Número Referencia</th>
+                                                        <th>N° de BL</th>
                                                         <th>Codigo Estandar </th>
                                                         <th>Envase/Estandar </th>
                                                         <th>Peso Neto </th>
@@ -260,16 +261,15 @@ include_once "../../assest/config/datosUrLP.php";
                                                         <th>Modalidad Venta</th>
                                                         <th>Clausula Venta</th>
                                                         <th>Tipo Flete</th>
-                                                        <th>Fecha Corte Documental</th>   
+                                                        <th>Fecha Corte Documental</th>
                                                         <th>Fecha ETD</th>
                                                         <th>Fecha ETA</th>
-                                                        <th>Fecha Real ETA</th>                                            
+                                                        <th>Fecha Real ETA</th>
                                                         <th>Tipo Contenedor</th>
-                                                        <th>BL/AWB</th>
                                                         <th>N° Contenedor</th>
                                                         <th>Días Estimados</th>
                                                         <th>Días Reales </th>
-                                                        <th>Destino Final </th>       
+                                                        <th>Destino Final </th>
                                                         <th>Semana Instructivo</th>
                                                         <th>Semana Corte Documental</th>
                                                         <th>Semana ETD</th>
@@ -545,6 +545,7 @@ include_once "../../assest/config/datosUrLP.php";
                                                             <tr class="text-center">
                                                                 <td> <?php echo $r['NUMERO_ICARGA']; ?>  </td>
                                                                 <td> <?php echo $r['NREFERENCIA_ICARGA']; ?>  </td>
+                                                                <td> <?php echo $r['CRT_ICARGA']; ?> </td>
                                                                 <td> <?php echo $CODIGOESTANDAR; ?></td>
                                                                 <td> <?php echo $NOMBREESTANTAR ?></td>
                                                                 <td> <?php echo number_format($NETOESTANTAR, 2, ".", "") ?></td>
@@ -577,7 +578,6 @@ include_once "../../assest/config/datosUrLP.php";
                                                                 <td> <?php echo $r['FECHAETA']; ?> </td>
                                                                 <td> <?php echo $r['FECHAETAREAL']; ?> </td>
                                                                 <td> <?php echo $NOMBRETCONTENEDOR; ?> </td>
-                                                                <td> <?php echo $r['BLAWB']; ?> </td>
                                                                 <td> <?php echo $NUMEROCONTENEDOR; ?> </td>
                                                                 <td> <?php echo $r['ESTIMADO']; ?> </td>
                                                                 <td> <?php echo $r['REAL']; ?> </td>
